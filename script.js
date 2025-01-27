04405791eb4e71a2622bef008c7fcd497b1926a7
VEgorOFF <vit3r@mail.ru>
2025-01-27 17:40:21 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,37 @@ function getApi() {
   let blockFetch = document.querySelectorAll(".blockFetch");
 
   for (let i = 0; i < blockFetch.length; i++) {
-    fetch("https://randomuser.me/api/")
-      .then((res) => res.json())
-      .then((data) => {
-        let firstName = data.results[0].name.first;
-        let lastName = data.results[0].name.last;
-        let picture = data.results[0].picture.large;
+    if (
+      localStorage.getItem(`firstName[${i}]`) !== null &&
+      localStorage.getItem(`lastName[${i}]`) !== null &&
+      localStorage.getItem(`picture[${i}]`) !== null
+    ) {
+      let divFirstName = blockFetch[i].querySelector(".name_author");
+      let divPhoto = blockFetch[i].querySelector(".avatar_author");
+      divFirstName.innerHTML = `${localStorage.getItem(
+        `firstName[${i}]`
+      )} ${localStorage.getItem(`lastName[${i}]`)}`;
+      divPhoto.innerHTML = `<img src="${localStorage.getItem(
+        `picture[${i}]`
+      )}">`;
+    } else {
+      fetch("https://randomuser.me/api/")
+        .then((res) => res.json())
+        .then((data) => {
+          let firstName = data.results[0].name.first;
+          let lastName = data.results[0].name.last;
+          let picture = data.results[0].picture.large;
 
-        let divFirstName = blockFetch[i].querySelector(".name_author");
-        let divPhoto = blockFetch[i].querySelector(".avatar_author");
-        divFirstName.innerHTML = `${firstName} ${lastName}`;
-        divPhoto.innerHTML = `<img src="${picture}">`;
-      });
+          localStorage.setItem(`firstName[${i}]`, firstName);
+          localStorage.setItem(`lastName[${i}]`, lastName);
+          localStorage.setItem(`picture[${i}]`, picture);
+
+          let divFirstName = blockFetch[i].querySelector(".name_author");
+          let divPhoto = blockFetch[i].querySelector(".avatar_author");
+          divFirstName.innerHTML = `${firstName} ${lastName}`;
+          divPhoto.innerHTML = `<img src="${picture}">`;
+        });
+    }
   }
 }
 getApi();
@@ -33,22 +52,36 @@ function sortCount() {
   let sortActual = document.getElementById("actual");
   let sortCountAnswers = document.getElementById("count_answers");
 
-  console.log(sortDate.querySelector("p"));
+  if (localStorage.getItem("sortName") !== null) {
+    sortName.innerHTML = localStorage.getItem("sortName");
+  } else {
+    sortName.innerHTML = sortActual.querySelector("p").textContent;
+  }
 
   sortDate.addEventListener("click", function () {
     sortName.innerHTML = sortDate.querySelector("p").textContent;
+    localStorage.setItem("sortName", sortDate.querySelector("p").textContent);
   });
 
   sortCountLikes.addEventListener("click", function () {
     sortName.innerHTML = sortCountLikes.querySelector("p").textContent;
+    localStorage.setItem(
+      "sortName",
+      sortCountLikes.querySelector("p").textContent
+    );
   });
 
   sortActual.addEventListener("click", function () {
     sortName.innerHTML = sortActual.querySelector("p").textContent;
+    localStorage.setItem("sortName", sortActual.querySelector("p").textContent);
   });
 
   sortCountAnswers.addEventListener("click", function () {
     sortName.innerHTML = sortCountAnswers.querySelector("p").textContent;
+    localStorage.setItem(
+      "sortName",
+      sortCountAnswers.querySelector("p").textContent
+    );
   });
 }
 
